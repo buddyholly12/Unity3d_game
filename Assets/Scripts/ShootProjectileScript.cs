ead810d5b972321226495a6c50d15c0afd884b4a
Agas <m26416077@john.petra.ac.id>
2021-03-20 22:44:21 +0700

    BasicShooting
    Added:
    - Basic Shooting
    - Basic Ammo Despawn
    Changed:
    - Camera positioning to be a bit more eye-level

---
+++ Assets/Scripts/ShootProjectileScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootProjectileScript : MonoBehaviour
+{
+    public Transform shootPoint;
+    public Rigidbody projectile;
+    public float projectileSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rigidbody BulletInstance;
+        if (Input.GetButtonDown("Fire1")) {
+            BulletInstance = Instantiate(projectile,shootPoint.position,shootPoint.rotation) as Rigidbody;
+            
+            BulletInstance.AddForce(shootPoint.forward * projectileSpeed);
+            
+        }
+    }
+}
