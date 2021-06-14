a0ba464689cff85d7657a0646fa1e509ef14a4fa
Agas <m26416077@john.petra.ac.id>
2021-06-14 20:40:32 +0700

    PlayerSpawn
    Added:
    PlayerSpawn Obj
    Changed:
    SpawnScript that only works if the spawn tracker is empty

--- Assets/Scripts/SpawnerScript.cs
+++ Assets/Scripts/SpawnerScript.cs
@@ -5,20 +5,23 @@ using UnityEngine;
 public class SpawnerScript : MonoBehaviour
 {
     public GameObject toSpawn;
+    public GameObject SpawnTracker;
     // Start is called before the first frame update
     void Start()
     {
-        Spawn(toSpawn);
+        //Spawn(toSpawn);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1") && SpawnTracker == null) {
+            SpawnAndTrack(toSpawn);
+        }
     }
 
-    void Spawn(GameObject prefabrication) {
-        Instantiate(prefabrication, transform);
+    void SpawnAndTrack(GameObject prefabrication) {
+        SpawnTracker = Instantiate(prefabrication, transform);
     }
 
 
