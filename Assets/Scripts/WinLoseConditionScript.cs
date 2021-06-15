b1b311181197cda7fc9da12b2eee1e5d0fb986fa
Agas <m26416077@john.petra.ac.id>
2021-06-16 00:28:55 +0700

    ImportToDesertSuccess
    Imported to DesertScene:
    Zombie Spawner
    Player Spawner
    WinLoseConditionScript

--- Assets/Scripts/WinLoseConditionScript.cs
+++ Assets/Scripts/WinLoseConditionScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class WinLoseConditionScript : MonoBehaviour
 {
-    public RandomSpawnerScript RandSpawnScr;
+    public RandomSpawnerScript ZombieSpawnScr;
     public SpawnerScript PlayerSpawnScr;
     public int zombieToKill = 5;
     public int lifeLimit = 1;
